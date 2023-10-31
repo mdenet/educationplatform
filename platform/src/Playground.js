bce5c58accd3abcc259e585a24ec3f7b820740ef
Will Barnett <will.barnett@kcl.ac.uk>
2023-10-31 17:27:23 +0000

    Removed unnecessary login box hide.

--- platform/src/Playground.js
+++ platform/src/Playground.js
@@ -59,7 +59,7 @@ document.getElementById("btnnologin").onclick= () => {
 if (!urlParamPrivateRepo()){
     // Public repo so no need to authenticate
     initialiseActivity();
-    PlaygroundUtility.hideLogin();
+    
 } else {
     PlaygroundUtility.showLogin();
 }
