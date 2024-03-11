7501674e02ed787fc96f474ad94bfaf3b832572e
Will Barnett <will.barnett@kcl.ac.uk>
2024-03-11 10:13:34 +0000

    Added windows testing edge launcher and github action.

--- platform/karma.conf.js
+++ platform/karma.conf.js
@@ -10,6 +10,7 @@ module.exports = function(config) {
         require('karma-jasmine'),
         require('karma-chrome-launcher'),
         require("karma-firefox-launcher"),
+        require("karma-edgium-launcher"),
         require("karma-safarinative-launcher"),
         require('karma-jasmine-html-reporter'),
         require('karma-junit-reporter')
