ecd0a210960d5ab4f82b1b51772364434097e3d5
AniadHossain <124696646+AniadHossain@users.noreply.github.com>
2025-03-07 23:23:38 +0000

    display extension icon and activity sidebar

--- mdenet-extension/src/web/extension.js
+++ mdenet-extension/src/web/extension.js
@@ -4,7 +4,7 @@ import * as vscode from 'vscode';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -13,7 +13,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('mdenet-extension.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('activities.refresh', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
