08264aaf1964f99b2c1cdffcdf895e24e68cf25c
AniadHossain <124696646+AniadHossain@users.noreply.github.com>
2025-03-07 23:54:37 +0000

    display files ending with activity.json or activity.yml on the activity side bar

---
+++ mdenet-extension/src/web/LocalRepoManager.js
@@ -0,0 +1,61 @@
+import * as vscode from 'vscode';
+
+export class LocalRepoManager {
+    static instance;
+
+    constructor() {
+        if (LocalRepoManager.instance) {
+            return LocalRepoManager.instance;
+        }
+
+        const workspaceFolders = vscode.workspace.workspaceFolders;
+        if (!workspaceFolders) {
+            console.log('No workspace is opened');
+            this.files = [];
+        } else {
+            this.rootUri = workspaceFolders[0].uri; // Use VS Code's Uri API
+            console.log('Root path:', this.rootUri.toString());
+            this.files = [];
+        }
+
+        LocalRepoManager.instance = this;
+    }
+
+    async initialize() {
+        if (!this.rootUri) {
+            return;
+        }
+
+        try {
+            const dirEntries = await vscode.workspace.fs.readDirectory(this.rootUri);
+            this.files = dirEntries
+                .filter(([file, type]) => type === vscode.FileType.File)
+                .map(([file]) => file)
+                .filter(file => file.endsWith('activity.json') || file.endsWith('activity.yml'));
+
+            console.log('Files:', this.files);
+        } catch (error) {
+            console.error('Error reading workspace directory:', error);
+            return;
+        }
+    }
+
+    getFiles() {
+        return this.files;
+    }
+
+    async fetchActivityFile(fileName) {
+        if (!this.rootUri) {
+            throw new Error("No workspace folder is open.");
+        }
+
+        try {
+            const fileUri = vscode.Uri.joinPath(this.rootUri, fileName);
+            const fileData = await vscode.workspace.fs.readFile(fileUri);
+            return new TextDecoder("utf-8").decode(fileData);
+        } catch (error) {
+            console.error(`Error reading file ${fileName}:`, error);
+            throw error;
+        }
+    }
+}
--- mdenet-extension/src/web/extension.js
+++ mdenet-extension/src/web/extension.js
@@ -1,14 +1,14 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { ActivityTreeDataProvider } from './providers/ActivityTreeDataProvider';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "mdenet-extension" is now active in the web extension host!');
+	const activityProvider = new ActivityTreeDataProvider();
+	vscode.window.registerTreeDataProvider('activities', activityProvider);
+	
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
